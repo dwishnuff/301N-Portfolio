689f9e90170cc1c8eaf9846dfe67c07ce74ce1e1
Danielle Beyer <dwishnuff@gmail.com>
2017-10-19 21:09:14 -0700

    created constructor object, updated CSS

---
+++ portfolio.js
@@ -0,0 +1,8 @@
+function Projects (projObj) {
+  this.title = projObj.title;
+  this.summary = projObj.summary;
+  this.link = projObj.link;
+  this.image = projObj.image;
+  this.datePub = projObj.datePub;
+  this.tech = projObj.tech;
+  }
