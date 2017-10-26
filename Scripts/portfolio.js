e2ffd74badf9e915ea775c7820b1b418fab4ecf0
Danielle Beyer <dwishnuff@gmail.com>
2017-10-25 20:57:25 -0700

    added sample projects, completed portfolio 2

--- Scripts/portfolio.js
+++ Scripts/portfolio.js
@@ -1,4 +1,6 @@
 
+var projects =[];
+
 //object constructor
 var Project = function (projObj) {
   this.title = projObj.title;
@@ -8,3 +10,35 @@ var Project = function (projObj) {
   this.publishedOn = projObj.publishedOn;
   this.techUsed = projObj.techUsed;
 }
+
+//clone template and fill template
+Project.prototype.toHtml = function() {
+  var $newProject = $('article.template').clone();
+
+  $newProject.removeClass('template');
+
+  $newProject.find("header h1 a").text(this.title).attr("href", this.link);
+  $newProject.find(".projInfo time").text(this.publishedOn);
+  $newProject.find(".tech").text($newProject.find(".tech").text()+this.techUsed);
+  $newProject.find(".projSummary img").html(this.image);
+  $newProject.find(".projSummary p").html(this.summary);
+
+  // Display the date as a relative number of 'days ago'
+  $newProject.find('.daysAgo').text('(' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago)');
+  $newProject.append('<hr>');
+
+  return $newProject;
+}
+
+//sort array of sample projects by date
+sampleProjects.sort(function(a,b) {
+return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+});
+
+sampleProjects.forEach(function(projectObject) {
+  projects.push(new Project(projectObject));
+});
+
+projects.forEach(function(projectSample) {
+  $('#projSection').append(projectSample.toHtml());
+});
---
+++ scripts/projects.js
@@ -0,0 +1,28 @@
+
+
+var sampleProjects = [
+  {
+    title:  'Sample Project 1 - Yahoo',
+    link:   'http://www.yahoo.com',
+    publishedOn: '2017-01-01',
+    techUsed: 'JavaScript, jQuery, HTML, CSS',
+    image:  'tbd',
+    summary:  '<p>Sample text to be added<p>'
+  },
+  {
+    title:  'Sample Project 2 - Google',
+    link:   'http://www.google.com',
+    publishedOn: '2017-02-01',
+    techUsed: 'JavaScript, jQuery, HTML, handlebars.js',
+    image:  'tbd',
+    summary:  '<p>Sample text to be added<p>'
+  },
+  {
+    title:  'Sample Project 3 - CNN',
+    link:   'http://www.cnn.com',
+    publishedOn: '2017-03-01',
+    techUsed: 'JavaScript, jQuery, HTML, handlebars.js',
+    image:  'tbd',
+    summary:  '<p>Sample text to be added<p>'
+  },
+];
