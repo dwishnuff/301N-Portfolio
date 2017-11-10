e04f7d8318a6739089f48227fd1db81236ee54a2
Danielle Beyer <dwishnuff@gmail.com>
2017-11-09 21:05:46 -0800

    updated with SQL server, data not loading

--- public/scripts/projectView.js
+++ public/scripts/projectView.js
@@ -40,45 +40,6 @@ projectView.handleTechFilter = function () {
     }
   })
 }
-//
-// projectView.handleAuthorFilter = function() {
-//   $('#author-filter').on('change', function() {
-//     // REVIEW: Inside this function, "this" is the element that triggered the event handler function we're
-//     //         defining. "$(this)" is using jQuery to select that element, so we can chain jQuery methods
-//     //         onto it.
-//     if ($(this).val()) {
-//       $('.article-body').hide();
-//       console.log($(this).val());
-//       $('article[data-author*="' + $(this).val() +'"] .article-body').fadeIn(1000);
-//       // TODO: If the select box was changed to an option that has a value, we need to hide all the articles,
-//       //       and then show just the ones that match for the author that was selected.
-//       //       Use an "attribute selector" to find those articles, and fade them in for the reader.
-//
-//     } else {
-//       $('.article-body').show();
-//       // TODO: If the select box was changed to an option that is blank, we should
-//       //       show all the articles, except the one article we are using as a template.
-//
-//     }
-//     $('#category-filter').val('');
-//   });
-// };
-//
-// projectView.handleCategoryFilter = function() {
-//   // TODO: Just like we do for #author-filter above, we should handle change events on the #category-filter element.
-//   //       When an option with a value is selected, hide all the articles, then reveal the matches.
-//   //       When the blank (default) option is selected, show all the articles, except for the template.
-//   //       Be sure to reset the #author-filter while you are at it!
-//   $('#category-filter').on('change', function() {
-//     if ($(this).val()) {
-//       $(".article-body").hide();
-//       $('article[data-category="' + $(this).val() +'"] .article-body').show();
-//     } else {
-//       $(".article-body").show();
-//     }
-//     $('#author-filter').val('');
-//   })
-// };
 
 projectView.handleMainNav = function() {
 
@@ -116,7 +77,5 @@ projectView.initIndexPage = function() {
   projectView.setTeasers();
   projectView.populateFilters();
   projectView.handleTechFilter();
-  // articleView.handleAuthorFilter();
-  // articleView.handleCategoryFilter();
   projectView.handleMainNav();
 };
